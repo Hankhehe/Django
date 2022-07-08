156d6d5c31399711ea74f9c685b499433d49a8d3
Hank <hz4154261@gmail.com>
2022-07-08 17:24:41 +0800

    fix

--- blog/Probe/urls.py
+++ blog/Probe/urls.py
@@ -1,9 +1,8 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.ProbeDashboard),
     path('Models', views.ProbeModelListView.as_view()),
     path('Probes', views.ProbeListVView.as_view()),
 ]
--- blog/Probe/views.py
+++ blog/Probe/views.py
@@ -3,13 +3,13 @@ from django.views import generic
 from .models import ProbeModelDetail,ProbeList
 # Create your views here.
 
-def index(request):
+def ProbeDashboard(request):
     P110Count = ProbeList.objects.filter(ModelID=6).count()
     P110PlusCount = ProbeList.objects.filter(ModelID=3).count()
     P220Count = ProbeList.objects.filter(ModelID=4).count()
     P360Count = ProbeList.objects.filter(ModelID=5).count()
     P500Count = ProbeList.objects.filter(ModelID=7).count()
-    return render(request,'Probe/templates/DashBoard.html'
+    return render(request,'Probe/templates/Probe_DashBoard.html'
     ,{'P110':P110Count,'P110Plus':P110PlusCount,'P220':P220Count,'P360':P360Count,'P560':P500Count}) 
 
 class ProbeModelListView(generic.ListView):
